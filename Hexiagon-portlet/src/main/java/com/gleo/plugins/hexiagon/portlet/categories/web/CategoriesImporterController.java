40754a704e560747a82fed1c31bd44d62d2b0712
Guillaume Lenoir <guillaume.lenoir@ext.betc.com>
2016-03-13 15:39:44 +0100

    refacto

--- Hexiagon-portlet/src/main/java/com/gleo/plugins/hexiagon/portlet/categories/web/CategoriesImporterController.java
+++ Hexiagon-portlet/src/main/java/com/gleo/plugins/hexiagon/portlet/categories/web/CategoriesImporterController.java
@@ -34,8 +34,6 @@ import java.util.Map;
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletException;
-import javax.portlet.RenderRequest;
-import javax.portlet.RenderResponse;
 
 /**
  * Portlet implementation class CategoriesImporterController
